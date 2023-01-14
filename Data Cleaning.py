1d8ce6889b32ac35643660eb4f6e99c12e791496
Rahul Raghavendra Choudhury <104511030+2020uce0047@users.noreply.github.com>
2023-01-14 20:26:37 +0530

    Add files via upload

---
+++ Data Cleaning.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('imdb_top_1000.csv')
+df = df.drop(columns = ['Poster_Link'])
+
+#Duration in min
+duration = df['Runtime'].apply(lambda x: x.split(' ')[0])
+df['Duration(min)'] = duration
+df['Duration(min)'].astype(int)
+df = df.drop(columns = ['Runtime'])
+
+
+value_count_genre = df['Genre'].apply(lambda x: len(x.split(',')))
+print(max(value_count_genre))
+Genre1 = df['Genre'].apply(lambda x: x.split(',')[0])
+Genre2 = df['Genre'].apply(lambda x: x.split(',')[1] if len(x.split(','))==2 else '')
+Genre3 = df['Genre'].apply(lambda x: x.split(',')[2] if len(x.split(','))==3 else '')
+
+df['Genre1'] = Genre1
+df['Genre2'] = Genre2
+df['Genre3'] = Genre3
+df = df.drop(columns = 'Genre')
+
+
+
+
+df = df.drop(df[(df['Certificate'] == 'TV-PG') |
+                (df['Certificate'] == 'GP') |
+                (df['Certificate'] == 'TV-14') |
+                (df['Certificate'] == '16') |
+                (df['Certificate'] == 'TV-MA') |
+                (df['Certificate'] == 'Unrated') |
+                (df['Certificate'] == 'U/A')].index)
+unique_cert = df['Certificate'].value_counts()
+print(unique_cert)
+
