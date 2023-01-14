371c7549bd02f5d3401bdd2a918ded3600dca0a6
Rahul Raghavendra Choudhury <104511030+2020uce0047@users.noreply.github.com>
2023-01-14 20:30:31 +0530

    Update data celaning.py

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -12,13 +12,13 @@ import numpy as np
 df = pd.read_csv('imdb_top_1000.csv')
 df = df.drop(columns = ['Poster_Link'])
 
-#Duration in min
+#Duration column in min
 duration = df['Runtime'].apply(lambda x: x.split(' ')[0])
 df['Duration(min)'] = duration
 df['Duration(min)'].astype(int)
 df = df.drop(columns = ['Runtime'])
 
-
+# Splitting multiple genre
 value_count_genre = df['Genre'].apply(lambda x: len(x.split(',')))
 print(max(value_count_genre))
 Genre1 = df['Genre'].apply(lambda x: x.split(',')[0])
@@ -30,8 +30,9 @@ df['Genre2'] = Genre2
 df['Genre3'] = Genre3
 df = df.drop(columns = 'Genre')
 
-
-
+#Dropping movies with certificate having value count(s) < 10
+unique_cert = df['Certificate'].value_counts()
+print(unique_cert)
 
 df = df.drop(df[(df['Certificate'] == 'TV-PG') |
                 (df['Certificate'] == 'GP') |
@@ -40,6 +41,5 @@ df = df.drop(df[(df['Certificate'] == 'TV-PG') |
                 (df['Certificate'] == 'TV-MA') |
                 (df['Certificate'] == 'Unrated') |
                 (df['Certificate'] == 'U/A')].index)
-unique_cert = df['Certificate'].value_counts()
-print(unique_cert)
+
 
